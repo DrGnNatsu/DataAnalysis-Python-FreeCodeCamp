493f991774c3268edec0c4d296ecaa80e70a8f83
DzNatsu <phphuong.career@gmail.com>
2024-08-06 19:09:04 +0700

    Finish Lecture_2
    Complete Lecture_2
    Update Lecture_1

--- DataAnalysisExampleA_B/Lecture_1.py
+++ DataAnalysisExampleA_B/Lecture_1.py
@@ -15,42 +15,56 @@ if __name__ == '__main__':
     print(f"{sales['Unit_Cost'].median()}\n")  # Median: is the middle value of the values
 
     sales['Unit_Cost'].plot(kind='box', vert=False, figsize=(14, 6))
+    plt.show()
+
     sales['Unit_Cost'].plot(kind='density', figsize=(14, 6))
+    plt.show()
 
     ax = sales['Unit_Cost'].plot(kind='density', figsize=(14, 6))  # kde
     ax.axvline(sales['Unit_Cost'].mean(), color='red')
     ax.axvline(sales['Unit_Cost'].median(), color='green')
+    plt.show()
 
     ax = sales['Unit_Cost'].plot(kind='hist', figsize=(14, 6))
     ax.set_ylabel('Number of Sales')
     ax.set_xlabel('dollars')
+    plt.show()
 
     print(sales['Age_Group'].value_counts())
     sales['Age_Group'].value_counts().plot(kind='pie', figsize=(7, 7))
+    plt.show()
 
     ax = sales['Age_Group'].value_counts().plot(kind='bar', figsize=(14, 14))
     ax.set_ylabel('Number of Sales')
+    plt.show()
 
     sales.plot(kind='scatter', x='Customer_Age', y='Revenue', figsize=(6, 6))
+    plt.show()
+
     sales.plot(kind='scatter', x='Revenue', y='Profit', figsize=(6, 6))
+    plt.show()
 
     ax = sales[['Profit', 'Age_Group']].boxplot(by='Age_Group', figsize=(10, 6))
     ax.set_ylabel('Profit')
+    plt.show()
 
     boxplot_cols = ['Year', 'Customer_Age', 'Order_Quantity', 'Unit_Cost', 'Unit_Price', 'Profit']
     print(sales[boxplot_cols].info())
     sales[boxplot_cols].plot(kind='box', subplots=True, layout=(2, 3), figsize=(14, 8))
+    plt.show()
 
     # Because the data is not clean, we need to clean it
     # We can not convert the column which have datatype object (String) to float or int
     object_columns = sales.select_dtypes(include=['object']).columns
     sales = sales.drop(columns=object_columns)
+
     corr = sales.corr()
     print(corr)
     fig = plt.figure(figsize=(8, 8))
     plt.matshow(corr, cmap='RdBu', fignum=fig.number)
     plt.xticks(range(len(corr.columns)), corr.columns, rotation='vertical')
     plt.yticks(range(len(corr.columns)), corr.columns)
+    plt.show()
 
     # =============================================================================================================
     sales['Revenue_per_Age'] = sales['Revenue'] / sales['Customer_Age']
@@ -58,7 +72,10 @@ if __name__ == '__main__':
     print(sales['Revenue_per_Age'].head())
 
     sales['Revenue_per_Age'].plot(kind='density', figsize=(14, 6))
+    plt.show()
+
     sales['Revenue_per_Age'].plot(kind='hist', figsize=(14, 6))
+    plt.show()
 
     # =============================================================================================================
     sales['Calculated_Cost'] = sales['Order_Quantity'] * sales['Unit_Cost']
@@ -67,23 +84,31 @@ if __name__ == '__main__':
     print(f"{(sales['Calculated_Cost'] != sales['Cost']).sum()}\n")
 
     sales.plot(kind='scatter', x='Calculated_Cost', y='Profit', figsize=(6, 6))
+    plt.show()
 
     # =============================================================================================================
     sales['Calculated_Revenue'] = sales['Cost'] + sales['Profit']
     print(f"{sales['Calculated_Revenue'].head()}\n")
     print(f"{(sales['Calculated_Revenue'] != sales['Revenue']).sum()}\n")
     print(f"{sales.head()}")
+
     sales['Revenue'].plot(kind='hist', bins=100, figsize=(14, 6))
+    plt.show()
+    
     print(f"{sales['Unit_Price'].head()}\n")
     sales['Unit_Price'] *= 1.03
     print(f"{sales['Unit_Price'].head()}\n")
 
     # =============================================================================================================
     print(f"{sales.loc[sales['State'] == 'Kentucky']}\n")
+
     print(f"{sales.loc[sales['Age_Group'] == 'Adults (35-64)', 'Revenue'].mean()}\n")
+
     print(f"{sales.loc[(sales['Age_Group'] == 'Youth (<25)') | (sales['Age_Group'] == 'Adults (35-64)')].shape[0]}\n")
-    print(f"{sales.loc[(sales['Age_Group'] == 'Adults (35-64)') & (sales['Country'] == 'United States'), 'Revenue'].mean()}\n")
+
+    print(f"{sales.loc[(sales['Age_Group'] == 'Adults (35-64)') & 
+                       (sales['Country'] == 'United States'), 'Revenue'].mean()}\n")
+
     print(f"{sales.loc[sales['Country'] == 'France', 'Revenue'].head()}\n")
     sales.loc[sales['Country'] == 'France', 'Revenue'] *= 1.1
     print(f"{sales.loc[sales['Country'] == 'France', 'Revenue'].head()}\n")
-    plt.show()
--- DataAnalysisExampleA_B/Lecture_2.py
+++ DataAnalysisExampleA_B/Lecture_2.py
@@ -29,15 +29,53 @@ if __name__ == "__main__":
     ''', conn, index_col='rental_id', parse_dates=['rental_date', 'return_date'])
 
     # ===============================================================================================================
-    # print(f"{df.head()}\n")
-    # print(f"{df.shape}\n")
-    # print(f"{df.info()}\n")
-    # print(f"{df.describe()}\n")
+    print(f"{df.head()}\n")
+    print(f"{df.shape}\n")
+    print(f"{df.info()}\n")
+    print(f"{df.describe()}\n")
 
     # ===============================================================================================================
     print(f"{df['film_rental_rate'].describe()}\n")
     print(f"{df['film_rental_rate'].mean()}\n")
     print(f"{df['film_rental_rate'].median()}\n")
-    print(f"{df['film_rental_rate'].plot(kind='box', vert=False, figsize=(14, 6))}\n")
 
+    print(f"{df['film_rental_rate'].plot(kind='box', vert=False, figsize=(14, 6))}\n")
     plt.show()
+
+    print(f"{df['film_rental_rate'].plot(kind='density', figsize=(14, 6))}\n")
+    plt.show()
+
+    ax = df['film_rental_rate'].value_counts().plot(kind='bar', figsize=(14, 6))
+    ax.set_ylabel('Number of Rentals')
+    plt.show()
+
+    # ===============================================================================================================
+    print(f"{df['film_rating'].value_counts()}\n")
+    df['rental_store_city'].value_counts().plot(kind='pie', figsize=(6, 6))
+    plt.show()
+
+    ax = df['rental_store_city'].value_counts().plot(kind='bar', figsize=(14, 6))
+    ax.set_ylabel('Number of Rentals')
+    plt.show()
+
+    # ===============================================================================================================
+    df['rental_gain_return'] = df['film_rental_rate'] / df['film_replacement_cost'] * 100
+    print(f"{df['rental_gain_return'].head()}\n")
+
+    df['rental_gain_return'].plot(kind='density', figsize=(14, 6))
+    print(f"{df['rental_gain_return'].mean().round(2)}\n")
+    print(f"{df['rental_gain_return'].median().round(2)}\n")
+    plt.show()
+
+    ax = df['rental_gain_return'].plot(kind='density', figsize=(14, 6))  # kde
+    ax.axvline(df['rental_gain_return'].mean(), color='red')
+    ax.axvline(df['rental_gain_return'].median(), color='green')
+    plt.show()
+
+    print(f"{df['film_title'].value_counts().mean()}\n")
+
+    # ===============================================================================================================
+    print(f"{df.loc[df['customer_lastname'] == 'HANSEN']}\n")
+    print(f"{df['film_replacement_cost'].max()}\n")
+    print(f"{df.loc[df['film_replacement_cost'] == df['film_replacement_cost'].max(), 'film_title'].unique()}\n")
+    print(f"{df.loc[(df['film_rating'] == 'PG') | (df['film_rating'] == 'PG-13')].shape[0]}\n")
